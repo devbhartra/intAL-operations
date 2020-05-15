62be6d10723baba83a8faebaae618fd97464f93c
Dev Bhartra <devbhartra183@gmail.com>
2020-05-15 14:36:13 +0530

    multiplication done

--- main.c
+++ main.c
@@ -32,12 +32,13 @@ char *intal_add(const char *intal1, const char *intal2)
     arr = (char *)malloc((size1 + size2) * sizeof(char));
 
     // converting int to char array
-    sprintf(arr, "%ld", sum);
+    sprintf(arr, "%d", sum);
     // printf("%s", arr);
     return arr;
 }
 
-int intal_compare(const char* intal1, const char* intal2){
+int intal_compare(const char *intal1, const char *intal2)
+{
     int size1 = strlen(intal1);
     int size2 = strlen(intal2);
     char *s1;
@@ -55,18 +56,20 @@ int intal_compare(const char* intal1, const char* intal2){
     {
         strncat(s2, &intal2[j], 1);
     }
-    
+
     int num1 = atoi(s1);
     int num2 = atoi(s2);
 
-    if (num1 == num2){
+    if (num1 == num2)
+    {
         return 0;
     }
-    else if(num1 > num2)
+    else if (num1 > num2)
     {
         return 1;
     }
-    else{
+    else
+    {
         return -1;
     }
 
@@ -74,7 +77,7 @@ int intal_compare(const char* intal1, const char* intal2){
     // arr = (char *)malloc((size1 + size2) * sizeof(char));
 
     // // converting int to char array
-    // sprintf(arr, "%ld", comp);
+    // sprintf(arr, "%d", comp);
     // // printf("%s", arr);
     // return arr;
 }
@@ -108,8 +111,36 @@ char *intal_diff(const char *intal1, const char *intal2)
     arr = (char *)malloc((size1 + size2) * sizeof(char));
 
     // converting int to char array
-    sprintf(arr, "%ld", diff);
+    sprintf(arr, "%d", diff);
     // printf("%s", arr);
+    return arr;
+}
+
+// return the product of 2 instals
+char *intal_multiply(const char *intal1, const char *intal2)
+{
+    int size1 = strlen(intal1);
+    int size2 = strlen(intal2);
+    char *s1;
+    char *s2;
+    s1 = (char *)malloc(size1 * sizeof(char));
+    s2 = (char *)malloc(size2 * sizeof(char));
+
+    for (int i = 0; i < strlen(intal1); i++)
+    {
+        strncat(s1, &intal1[i], 1);
+    }
+    for (int j = 0; j < strlen(intal2); j++)
+    {
+        strncat(s2, &intal2[j], 1);
+    }
+    int product = atoi(s1) * atoi(s2);
+
+    char *arr;
+    arr = (char *)malloc((size1 + size2) * sizeof(char));
+
+    // converting int to char array
+    sprintf(arr, "%d", product);
     return arr;
 }
 
@@ -121,14 +152,17 @@ int main()
     char intal3[] = {'3', '5', '\0'};
     char intal4[] = {'3', '5', '\0'};
 
-    // char *temp1 = intal_add(intal1, intal2);
-    // printf("%s", temp1);
-    // printf("%d", sum);
+    char *temp1 = intal_add(intal1, intal2);
+    printf("%s", temp1);
+
 
     // char *temp2 = intal_diff(intal3, intal4);
     // printf("%s", temp2);
 
-    int compare = intal_compare(intal1, intal4);
-    printf("%d", compare);
+    // int compare = intal_compare(intal1, intal4);
+    // printf("%d", compare);
+
+    // char *temp3 = intal_multiply(intal1, intal4);
+    // printf("%s", temp3);
     return 0;
 }
