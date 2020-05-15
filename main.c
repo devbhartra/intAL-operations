6980bca83b5e2b44b287739f4c76f49544101b66
Dev Bhartra <devbhartra183@gmail.com>
2020-05-15 14:01:48 +0530

    Comparision done

--- main.c
+++ main.c
@@ -37,6 +37,48 @@ char *intal_add(const char *intal1, const char *intal2)
     return arr;
 }
 
+int intal_compare(const char* intal1, const char* intal2){
+    int size1 = strlen(intal1);
+    int size2 = strlen(intal2);
+    char *s1;
+    char *s2;
+    s1 = (char *)malloc(size1 * sizeof(char));
+    s2 = (char *)malloc(size2 * sizeof(char));
+
+    // int greater;
+    // (size1 > size2) ?  (greater = size1) : (greater = size2);
+    for (int i = 0; i < strlen(intal1); i++)
+    {
+        strncat(s1, &intal1[i], 1);
+    }
+    for (int j = 0; j < strlen(intal2); j++)
+    {
+        strncat(s2, &intal2[j], 1);
+    }
+    
+    int num1 = atoi(s1);
+    int num2 = atoi(s2);
+
+    if (num1 == num2){
+        return 0;
+    }
+    else if(num1 > num2)
+    {
+        return 1;
+    }
+    else{
+        return -1;
+    }
+
+    // char *arr;
+    // arr = (char *)malloc((size1 + size2) * sizeof(char));
+
+    // // converting int to char array
+    // sprintf(arr, "%ld", comp);
+    // // printf("%s", arr);
+    // return arr;
+}
+
 // Returns the absolute difference of two intals.
 char *intal_diff(const char *intal1, const char *intal2)
 {
@@ -71,6 +113,7 @@ char *intal_diff(const char *intal1, const char *intal2)
     return arr;
 }
 
+// Driver to test the above functions, remove before submission!!
 int main()
 {
     char intal1[] = {'1', '2', '\0'};
@@ -82,7 +125,10 @@ int main()
     // printf("%s", temp1);
     // printf("%d", sum);
 
-    char *temp2 = intal_diff(intal3, intal4);
-    printf("%s", temp2);
+    // char *temp2 = intal_diff(intal3, intal4);
+    // printf("%s", temp2);
+
+    int compare = intal_compare(intal1, intal4);
+    printf("%d", compare);
     return 0;
 }
