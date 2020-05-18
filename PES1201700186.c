4f1e178ff956b586be1d57d6a00cd477dcb2433b
Dev Bhartra <devbhartra183@gmail.com>
2020-05-19 00:34:59 +0530

    Single digit multiplication works

--- PES1201700186.c
+++ PES1201700186.c
@@ -72,7 +72,6 @@ char *intal_add(const char *intal1, const char *intal2)
     int carry = 0;
     int sum = 0;
     int j = greater;
-    printf("%d <--- greater\n", greater);
     for (int i = greater - 1; i >= 0; i--)
     {
         sum = (intArr1[i]) + (intArr2[i]) + carry;
@@ -137,8 +136,6 @@ char *intal_diff(const char *intal1, const char *intal2)
     int size2 = strlen(intal2);
 
     int greater = MAX(size1, size2);
-    printf("%d <-- greater\n", greater);
-
     int *intArr1;
     intArr1 = char2int(intal1, greater, size1);
     int *intArr2;
@@ -169,7 +166,7 @@ char *intal_diff(const char *intal1, const char *intal2)
     int diff = 0;
 
     // find bigger number:
-    int comp = intal_compare(intal1,intal2);
+    int comp = intal_compare(intal1, intal2);
 
     if (comp == 1)
     {
@@ -186,7 +183,8 @@ char *intal_diff(const char *intal1, const char *intal2)
         }
     }
     // both are equal
-    else if(comp == 0){
+    else if (comp == 0)
+    {
         diffArr[0] = 0;
     }
     // intal2 is greater
@@ -217,4 +215,77 @@ char *intal_diff(const char *intal1, const char *intal2)
     }
     return charArr;
     // return diffArr;
+}
+
+char *intal_multiply(const char *intal1, const char *intal2)
+{
+
+    int size1 = strlen(intal1);
+    int size2 = strlen(intal2);
+
+    int greater = MAX(size1, size2);
+
+    int *intArr1;
+    intArr1 = char2int(intal1, greater, size1);
+    int *intArr2;
+    intArr2 = char2int(intal2, greater, size2);
+
+    int total = size2 + size1;
+    int *prodArr;
+    prodArr = (int *)malloc(total * sizeof(int)); // need to strip
+
+    // both char intals are now int arrays
+    int carry = 0;
+    // int x = total - 1;
+    // int shift = 0;
+    // int prod;
+
+    // intal1 is bigger
+    // if (greater == size1)
+    // {
+    // int limit = greater - size2;
+    for (int i = greater - 1; i >= 0; i--) // the number below (intArr2)
+    {
+        for (int j = greater - 1; j >= 0; j--)
+        { // the number on top (intArr1)
+            prodArr[i + j] = prodArr[i + j] + (intArr2[i] * intArr1[j]);
+        }
+    }
+    // }
+    for (int i = 0; i <= total; i++)
+    {
+        printf("%d ", prodArr[i]);
+    }
+
+    int *resArray = (int *)malloc(total * sizeof(int));
+    for (int i = total; i >= 0; i--)
+    {
+        resArray[i] = (prodArr[i] % 10) + carry;
+        carry = prodArr[i] / 10;
+    }
+    // for (int i = total; i >= 0; i--)
+    // {
+    //     int digit = prodArr[i] % 10;
+    //     int carry = prodArr[i] / 10;
+    //     if ((i + 1) < total)
+    //     {
+    //         prodArr[i + 1] += carry;
+    //     }
+    //     resArray[i] += digit;
+    // }
+
+    printf("\n");
+    for (int i = 0; i <= total; i++)
+    {
+        printf("%d ", resArray[i]);
+    }
+    printf("\n");
+
+    // char *charArr = (char *)malloc(greater * sizeof(char));
+    // charArr = int2char(prodArr, greater);
+    // if (strlen(charArr) > 0 && charArr[0] == '0')
+    // {
+    //     charArr += 1;
+    // }
+    return " ";
 }
